f2e7b584e54f5e895846645908603c6cdd19eb51
colin-xid <colin@protopie.io>
2025-10-01 17:26:43 +0900

    ✨ Bootcamp 2018-1day (#1)
    * feat: bootcamp part 1
    
    * feat: bootcamp part2
    
    * refactor: stream 파서에서 단일 파서로 변경
    
    * chore: part2 정답 주석 추가
    
    * chore: 주석 설명 수정
    
    * refactor: 코드 리뷰 (doc string) 적용

--- clojure/src/aoc2018_1.clj
+++ clojure/src/aoc2018_1.clj
@@ -1,12 +1,43 @@
-(ns aoc2018-1)
+(ns aoc2018-1
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+;; 공통 함수
+(defn- parse-int 
+  "trim 과 함께 문제열을 정수로 변환"
+  [string]
+  (Integer/parseInt (str/trim string)))
+
+(defn- read-lines 
+  "resource-path 에 있는 파일을 읽어 정수 시퀀스로 반환"
+  [resource-path]
+  (->> (slurp (io/resource resource-path))
+       (str/split-lines)
+       (remove str/blank?)
+       (map parse-int)))
 
 ;; 파트 1
 ;; 주어진 입력의 모든 숫자를 더하시오.
 ;; 예) +10 -2 -5 +1 이 입력일 경우 4를 출력
 
+(comment
+  (println (reduce + (read-lines "2018_1.txt")))) ; 595
 
 ;; 파트 2
 ;; 주어진 입력의 숫자를 더할 때 마다 나오는 숫자 중, 처음으로 두번 나오는 숫자를 리턴하시오.
 ;; 예) +3, +3, +4, -2, -4 는 10이 처음으로 두번 나오는 숫자임.
 ;; 0 -> 3 (+3) -> 6 (+3) -> 10(+4) -> 8(-2) -> 4(-4) -> 7(+3) -> 10(+3) -> ...
 
+(defn first-duplicated-sum
+  "set 에 누적 합을 기록하고, 새로운 합이 set 에 포함되어 있다면 처음으로 두번 나오는 숫자로 판단하여 리턴함"
+  [resource-path]
+  (let [nums (cycle (read-lines resource-path))]
+    (loop [result-set #{}
+           [num & more] (reductions + nums)]
+      (if (result-set num)
+        num
+        (recur (conj result-set num) more)))))
+
+(comment
+  (println (first-duplicated-sum "2018_1.txt"))) ; 80598
