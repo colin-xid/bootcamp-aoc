60f77268284c527253726623d8c42751f333df54
colin-xid <colin@protopie.io>
2025-10-10 14:11:59 +0900

    feat: part1

--- clojure/src/aoc2018_3.clj
+++ clojure/src/aoc2018_3.clj
@@ -1,5 +1,26 @@
-(ns aoc2018_3)
+(ns aoc2018_3
+  (:require
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
 
+(defn- read-lines
+  "resource-path 에 있는 파일을 읽어 문자열 시퀀스로 반환"
+  [resource-path]
+  (->> (slurp (io/resource resource-path))
+       (str/split-lines)
+       (remove str/blank?)))
+
+(defn- parse-claim
+  "#1 @ 393,863: 11x29 를 {:id :x :y :width :height} 로 변환하는 함수"
+  [line]
+  (let [[_ id x y width height] (re-matches
+                                 #"#(\d+) @ (\d+),(\d+): (\d+)x(\d+)"
+                                 line)]
+    {:id (parse-long id)
+     :x (parse-long x)
+     :y (parse-long y)
+     :width (parse-long width)
+     :height (parse-long height)}))
 
 ;; 파트 1
 ;; 다음과 같은 입력이 주어짐.
@@ -23,7 +44,24 @@
 ;; 여기서 XX는 ID 1, 2, 3의 영역이 두번 이상 겹치는 지역.
 ;; 겹치는 지역의 갯수를 출력하시오. (위의 예시에서는 4)
 
+(defn- expand-points
+  "width 와 height 를 이용하여 모든 천의 좌표를 {[x, y] 1} 로 변환하는 함수"
+  [{:keys [_ x y width height]}]
+  (for [point-x (range x (+ x width))
+        point-y (range y (+ y height))]
+    {[point-x point-y] 1}))
 
+(defn count-overlapping-fabric
+  "중복된 천의 넓이를 계산하는 함수"
+  []
+  (->> (read-lines "2018_3.txt")
+       (map parse-claim)
+       (mapcat expand-points)
+       (apply merge-with +)
+       (filter (fn [[_ v]] (> v 1)))
+       (count)))
+
+(comment (println (count-overlapping-fabric)))
 
 
 ;; 파트 2
