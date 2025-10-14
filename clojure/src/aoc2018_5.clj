61237413f4324e5086085aa386282804647136c5
colin-xid <colin@protopie.io>
2025-10-14 15:36:01 +0900

    refactor: 함수명을 도메인에 맞게 변경

--- clojure/src/aoc2018_5.clj
+++ clojure/src/aoc2018_5.clj
@@ -14,12 +14,12 @@
 
 ;; 주어진 input 에서 최종으로 남는 문자열을 리턴하시오.
 
-(defn- read-polymer-line
+(defn- read-polymer
   "리소스 파일에서 폴리머 한 줄을 읽어 문자열로 반환한다."
   [resource-path]
   (slurp (io/resource resource-path)))
 
-(defn- char-range
+(defn- unit-range
   "캐릭터 start 에서 end 까지 포함하는 캐릭터 시퀀스를 생성한다"
   [start end]
   (->> (range (int start) (inc (int end)))
@@ -32,48 +32,51 @@
   (->> (mapv str range1 range2)
        (str/join "|")))
 
-(defn- a-Z-pattern
+(defn- reaction-pattern
   "대소문자만 다른 동일 문자 쌍(예: aA, Aa, bB, Bb, …)에 매치하는 정규식을 생성해 반환한다.
    생성되는 결과는 aA|bB|…|Aa|Bb|… 형태이다."
   []
-  (let [a-z (char-range \a \z)
-        A-Z (char-range \A \Z)
+  (let [a-z (unit-range \a \z)
+        A-Z (unit-range \A \Z)
         a-A-pair (make-pair-pattern a-z A-Z)
         A-a-pair (make-pair-pattern A-Z a-z)]
     (-> (str a-A-pair "|" A-a-pair)
         re-pattern)))
 
-(defn find-resulting-polymer-length
+(defn fully-polymer-length
   [polymer]
-  (let [pattern (a-Z-pattern)]
-    (-> (loop [polymer polymer]
-          (if (re-find pattern polymer)
-            (recur (str/replace polymer pattern ""))
-            polymer))
+  (let [pattern (reaction-pattern)]
+    (-> (loop [result polymer]
+          (if (re-find pattern result)
+            (recur (str/replace result pattern ""))
+            result))
         (count))))
 
-(find-resulting-polymer-length (read-polymer-line "2018_5.txt"))
-
+(comment
+  (->> (read-polymer "2018_5.txt")
+       (fully-polymer-length)))
 
 ;; 파트 2
 ;; 주어진 문자열에서 한 유닛 (대문자와 소문자)을 전부 없앤 후 반응시켰을 때, 가장 짧은 문자열의 길이를 리턴하시오.
 ;; 예를 들어 dabAcCaCBAcCcaDA 에서 a/A를 없애고 모두 반응시키면 dbCBcD가 되고 길이는 6인데 비해,
 ;; 같은 문자열에서 c/C를 없애고 모두 반응시키면 daDA가 남고 길이가 4이므로 4가 가장 짧은 길이가 됨.
 
-(defn- remove-char
+(defn- remove-unit
   "주어진 polymer 문자열에서 특정 문자를 대소문자구분 없이 제거하여 새로운 문자열을 반환한다."
-  [polymer character]
-  (let [pattern (re-pattern (str "(?i)" character))]
+  [polymer unit]
+  (let [pattern (re-pattern (str "(?i)" unit))]
     (str/replace polymer pattern "")))
 
-(defn find-shortest-polymer-length
+(defn shortest-polymer-length
   [polymer]
-  (->> (char-range \a \z)
+  (->> (unit-range \a \z)
        (map str)
-       (reduce (fn [before character]
-                 (-> (remove-char polymer character)
-                     find-resulting-polymer-length
-                     (min before)))
+       (reduce (fn [current-min unit]
+                 (-> (remove-unit polymer unit)
+                     fully-polymer-length
+                     (min current-min)))
                Integer/MAX_VALUE)))
 
-(find-shortest-polymer-length (read-polymer-line "2018_5.txt"))
+(comment
+  (->> (read-polymer "2018_5.txt")
+       (shortest-polymer-length)))
